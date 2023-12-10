f5a785f002ec1bf653d8a0e6f9fe2d8f2b61e797
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-12-10 11:25:42 +1100

    styled-components, framer-motion, vitest, react-testing library installed. Test config setup.

---
+++ src/components/Accordion/Accordion.test.jsx
@@ -0,0 +1,15 @@
+import {describe, it, expect} from 'vitest';
+import {render, screen} from '@testing-library/react';
+
+import App from '../../App.jsx';
+
+describe("something to be truthy", () => {
+  it('true should be true', () => {
+    expect(true).toBe(true);
+  })
+
+  it('should render app title', () => {
+    render(<App/>)
+    expect(screen.getByRole("heading").textContent).toMatch(/hello world/i);
+  });
+})
